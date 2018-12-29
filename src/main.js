b77a6852a5ccd9a6d319905b3041bf6879891d87
Anton Zdanov <azdanov@pm.me>
2018-12-29 22:15:18 +0200

    feat(main): add firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+import firebaseApp from "firebase/app";
 import Vue from "vue";
 import App from "./App.vue";
+import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+firebaseApp.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
+});
 
 new Vue({
   router,
