d6cc17b1a2530c89823232ab92239e7306ecc534
Anton Zdanov <azdanov@pm.me>
2018-12-29 22:12:17 +0200

    feat(Login): add login component

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="pt-2">Login</h2>
+
+    <button
+      class="bg-red hover:bg-red-dark text-white font-bold py-3 px-5 rounded-full text-lg shadow no-underline mt-6"
+    >
+      with Google
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style scoped></style>
