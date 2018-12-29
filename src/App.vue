12ac94ef42265e9ecf953689b6bfa3554dde9c07
Anton Zdanov <azdanov@pm.me>
2018-12-29 22:14:56 +0200

    feat(App): extract navbar, add tailwind.css

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="container text-black mx-auto antialiased font-sans text-center">
+    <NavBar></NavBar>
+    <RouterView />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+// import NProgress from "nprogress";
+// import nativeToast from "native-toast";
+import NavBar from "@/components/TheNavBar";
+
+export default {
+  components: {
+    NavBar
   }
-}
+};
+</script>
+
+<style>
+/* @import "~nprogress/nprogress.css"; */
+/* @import "~native-toast/dist/native-toast.css"; */
+@import "./styles/tailwind.css";
 </style>
