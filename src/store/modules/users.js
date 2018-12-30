acc00f61072b117ddff61d8218b29c2534ca5f96
Anton Zdanov <azdanov@pm.me>
2018-12-30 23:07:31 +0200

    feat(users): add functionality to fetchAllUsers, deleteUser

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -3,11 +3,17 @@ import firebase from "firebase";
 
 export default {
   state: { users: {} },
+
   mutations: {
     setUser(state, { id, user }) {
       Vue.set(state.users, id, user);
+    },
+
+    deleteUser(state, { id }) {
+      Vue.delete(state.users, id);
     }
   },
+
   actions: {
     createUser({ state, commit }, { id, email, name, avatar = null }) {
       return new Promise(resolve => {
@@ -30,6 +36,21 @@ export default {
       });
     },
 
+    deleteUser({ state, commit }, { id }) {
+      console.log(id);
+      return new Promise(resolve => {
+        firebase
+          .database()
+          .ref("users")
+          .child(id)
+          .remove()
+          .then(() => {
+            commit("deleteUser", { id });
+            resolve(state.users);
+          });
+      });
+    },
+
     fetchUser: ({ state, commit }, { id }) => {
       console.log("🛩‍", id);
       return new Promise(resolve => {
@@ -43,6 +64,23 @@ export default {
               user: snapshot.val()
             });
             resolve(state.users[id]);
+          });
+      });
+    },
+
+    fetchAllUsers: ({ state, commit }) => {
+      console.log("✈️‍‍‍", "all users");
+      return new Promise(resolve => {
+        return firebase
+          .database()
+          .ref("users")
+          .once("value", snapshot => {
+            const users = snapshot.val();
+            Object.entries(users).forEach(userEntry => {
+              const [id, user] = userEntry;
+              commit("setUser", { id, user });
+            });
+            resolve(state.users);
           });
       });
     }
