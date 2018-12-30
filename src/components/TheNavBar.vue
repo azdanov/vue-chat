c034a9097e94b4797b79b766191b7c2a44905c75
Anton Zdanov <azdanov@pm.me>
2018-12-30 12:39:43 +0200

    feat(TheNavBar): use named routes, use state for auth/guest

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,33 +1,50 @@
 <template>
-  <div v-if="show" class="my-4 text-grey-darkest flex justify-end">
-    <div>
+  <div class="my-4 text-grey-darkest flex justify-end">
+    <div v-if="!auth">
       <RouterLink
-        to="/"
+        :to="{ name: 'home' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
         exact-active-class="text-green-dark shadow-inner"
         >Home</RouterLink
       ><RouterLink
-        to="/about"
+        :to="{ name: 'about' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
         exact-active-class="text-green-dark shadow-inner"
         >About</RouterLink
       ><RouterLink
-        to="/login"
+        :to="{ name: 'login' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
         exact-active-class="text-green-dark shadow-inner"
         >Login</RouterLink
+      >
+    </div>
+    <div v-else>
+      <RouterLink
+        :to="{ name: 'chat' }"
+        class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
+        exact-active-class="text-green-dark shadow-inner"
+        >Chat</RouterLink
+      ><RouterLink
+        :to="{ name: 'about' }"
+        class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
+        exact-active-class="text-green-dark shadow-inner"
+        >About</RouterLink
+      ><RouterLink
+        :to="{ name: 'logout' }"
+        class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
+        exact-active-class="text-green-dark shadow-inner"
+        >Logout</RouterLink
       >
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "NavBar",
   computed: {
-    show() {
-      return ["/", "/about", "/login"].includes(this.$route.path);
-    }
+    ...mapState({ auth: state => state.auth.authId })
   }
 };
 </script>
