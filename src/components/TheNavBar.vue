48b34b5979699c64d97b74d698e09186ed0fef47
Anton Zdanov <azdanov@pm.me>
2018-12-30 19:55:58 +0200

    feat(TheNavBar): remove shadow

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -4,17 +4,17 @@
       <RouterLink
         :to="{ name: 'home' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >Home</RouterLink
       ><RouterLink
         :to="{ name: 'about' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >About</RouterLink
       ><RouterLink
         :to="{ name: 'login' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >Login</RouterLink
       >
     </div>
@@ -22,17 +22,17 @@
       <RouterLink
         :to="{ name: 'chat' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >Chat</RouterLink
       ><RouterLink
         :to="{ name: 'about' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >About</RouterLink
       ><RouterLink
         :to="{ name: 'logout' }"
         class="text-grey-darkest hover:bg-grey-lightest p-2 no-underline"
-        exact-active-class="text-green-dark shadow-inner"
+        exact-active-class="text-green-dark"
         >Logout</RouterLink
       >
     </div>
