0a8b844016b358a428c0d8d7168a48e68a13fa6c
Anton Zdanov <azdanov@pm.me>
2018-12-30 12:41:33 +0200

    fix(main): use prefix for vue app

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 firebaseApp.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_ID
 });
 
 new Vue({
