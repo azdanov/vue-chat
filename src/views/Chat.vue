2e3ca672c91a505704b4b16835e6fdb9b2f5d1e6
Anton Zdanov <azdanov@pm.me>
2018-12-30 20:45:58 +0200

    feat(chat): add NewMessage and UsersOnline

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,10 +1,23 @@
 <template>
-  <h2>Chat</h2>
+  <div class="w-full max-w-xs">
+    <NewMessage @newMessage="submit"></NewMessage>
+    <hr class="my-4" />
+    <UsersOnline></UsersOnline>
+  </div>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage";
+import UsersOnline from "@/components/UsersOnline";
+
 export default {
-  name: "Chat"
+  name: "Chat",
+  components: { UsersOnline, NewMessage },
+  methods: {
+    submit(message) {
+      console.log("✍🏻", message);
+    }
+  }
 };
 </script>
 
