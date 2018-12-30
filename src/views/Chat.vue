84cee2855b98c8cb40170132357c8eb3000c7f9f
Anton Zdanov <azdanov@pm.me>
2018-12-30 23:01:43 +0200

    feat(Chat): add actions, add events to lifecycle

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -7,13 +7,33 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import firebase from "firebase";
 import NewMessage from "@/components/NewMessage";
 import UsersOnline from "@/components/UsersOnline";
+
+const usersRef = firebase.database().ref("users");
 
 export default {
   name: "Chat",
   components: { UsersOnline, NewMessage },
+  created() {
+    usersRef.on("child_added", snapshot => {
+      console.log("🧔🏻 online", snapshot.key);
+      this.fetchUser({ id: snapshot.key });
+    });
+
+    usersRef.on("child_removed", snapshot => {
+      console.log("🧔🏻 offline", snapshot.key);
+      this.deleteUser({ id: snapshot.key });
+    });
+  },
+  beforeDestroy() {
+    usersRef.off("child_added");
+    usersRef.off("child_removed");
+  },
   methods: {
+    ...mapActions(["fetchUser", "deleteUser"]),
     submit(message) {
       console.log("✍🏻", message);
     }
