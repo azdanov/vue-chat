4a1df3afe11b754c13ef5d73342b41ee70aabc78
Anton Zdanov <azdanov@pm.me>
2018-12-30 23:02:45 +0200

    feat(UsersOnline): fetch users on created, iterate over users

--- src/components/UsersOnline.vue
+++ src/components/UsersOnline.vue
@@ -2,26 +2,19 @@
   <form class="bg-white shadow rounded px-8 py-4">
     <p class="block text-grey-darker text-sm font-bold mb-2">Users Online</p>
     <div class="flex flex-col text-left">
-      <div class="flex items-center justify-start my-2">
+      <div
+        v-for="(user, key) in online"
+        :key="key"
+        class="flex items-center justify-start my-2"
+      >
         <img
           class="w-10 h-10 rounded-full mr-4"
-          src="http://i.pravatar.cc/300?img=4"
-          alt="Avatar of Dr. Johannes Sölzer"
+          :src="user.avatar"
+          :alt="`Avatar of ${user.name}`"
         />
         <div class="text-sm">
-          <p class="text-black leading-none">Dr. Johannes Sölzer</p>
-          <p class="text-grey-dark">benjaminpons@tessier.fr</p>
-        </div>
-      </div>
-      <div class="flex items-center justify-start my-2">
-        <img
-          class="w-10 h-10 rounded-full mr-4"
-          src="http://i.pravatar.cc/300?img=3"
-          alt="Avatar of Jonathan Reinink"
-        />
-        <div class="text-sm">
-          <p class="text-black leading-none">Jonathan Reinink</p>
-          <p class="text-grey-dark">luis03@pujadas.com</p>
+          <p class="text-black leading-none">{{ user.name }}</p>
+          <p class="text-grey-dark">{{ user.email }}</p>
         </div>
       </div>
     </div>
@@ -29,8 +22,18 @@
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 export default {
-  name: "UsersOnline"
+  name: "UsersOnline",
+  computed: {
+    ...mapState({ online: state => state.users.users })
+  },
+  created() {
+    this.fetchAllUsers();
+  },
+  methods: {
+    ...mapActions(["fetchAllUsers"])
+  }
 };
 </script>
 
