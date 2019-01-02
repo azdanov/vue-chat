b31da05d121f7a59171e98a765886559a795b591
Anton Zdanov <azdanov@pm.me>
2019-01-02 15:30:01 +0200

    feat(About): add description

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,21 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <div class="text-left leading-loose">
+    <div class="mx-auto w-full max-w-md">
+      <h1>About</h1>
+      <p class="leading-normal font-serif">
+        A toy project to try out modern technologies, and build a 'serverless'
+        web app. Authentication is handled by Google and Github providers inside
+        Firebase. Realtime Database allows tracking of new users, and messages
+      </p>
+      <p>Tailwind CSS was used for rapid prototyping of the UI.</p>
+      <h2>Technologies used:</h2>
+      <ul>
+        <li><a href="https://vuejs.org/">Vue.js</a></li>
+        <li><a href="https://router.vuejs.org/">Vue Router</a></li>
+        <li><a href="https://vuex.vuejs.org/">Vuex</a></li>
+        <li><a href="https://firebase.google.com/">Firebase</a></li>
+        <li><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
